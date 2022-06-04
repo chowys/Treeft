c78a590137c6a379d738fad47c577ce58ef84682
Chow Yee Sen <yeesenchow8@gmail.com>
2022-06-04 19:34:57 +0800

    Added Tabs, Drawwers and modified Logout button

--- code/lib/Page/Homescreen.dart
+++ code/lib/Page/Homescreen.dart
@@ -16,17 +16,68 @@ class Homescreen extends StatefulWidget {
 class _HomescreenState extends State<Homescreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          child: Text("Logout"),
-          onPressed: () {
-            FirebaseAuth.instance.signOut().then((value) {
-              print("Signed out");
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => LogIn()));
-            });
-          },
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          //Navigation Tabs
+          appBar: AppBar(
+            iconTheme: IconThemeData(color: Colors.black), //icon colour
+            backgroundColor: Color(0xffFFDE59),
+            bottom: TabBar(
+              indicator: BoxDecoration(
+                  borderRadius: BorderRadius.circular(50),
+                  color: Color.fromARGB(255, 185, 161, 64)),
+              tabs: [
+                Tab(icon: Icon(Icons.shopping_cart)),
+                Tab(icon: Icon(Icons.videogame_asset)),
+                Tab(icon: Icon(Icons.admin_panel_settings)),
+              ],
+            ),
+            title: const Text('Treeft'),
+          ),
+
+//Drawer
+          drawer: Drawer(
+            // Add a ListView to the drawer. This ensures the user can scroll
+            // through the options in the drawer if there isn't enough vertical
+            // space to fit everything.
+            child: ListView(
+              // Important: Remove any padding from the ListView.
+              padding: EdgeInsets.zero,
+              children: [
+                const DrawerHeader(
+                  decoration: BoxDecoration(
+                    color: Color(0xffFFDE59),
+                  ),
+                  child: Text('Menu'),
+                ),
+                ListTile(
+                  title: const Text('Logout'),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    FirebaseAuth.instance.signOut().then((value) {
+                      print("Signed out");
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => LogIn()));
+                    });
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
+          ),
+
+//Content for each Tab, can try to redo or abstract to another screen
+          body: const TabBarView(
+            children: [
+              Center(child: Text('Shopping List Page')),
+              Center(child: Text('Treeft tree')),
+              Center(child: Text('Settings')),
+            ],
+          ),
         ),
       ),
     );
